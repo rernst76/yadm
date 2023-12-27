06ae8ac5efb21815f5872a0424280355067b21c7
re76 <rernst76@gmail.com>
2023-12-27 11:31:48 -0500

    Add window picker

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -28,4 +28,15 @@ return {
       ["yaml.ansible"] = true,
     },
   },
+  {
+    "s1n7ax/nvim-window-picker",
+    name = "window-picker",
+    event = "VeryLazy",
+    version = "2.*",
+    config = function()
+      require("window-picker").setup({
+        hint = "floating-big-letter",
+      })
+    end,
+  },
 }
